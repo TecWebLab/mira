b66440f4eca14f6c365b1f0829ce3a79455977ff
Ezequiel Bertti <ebertti@gmail.com>
2014-11-08 01:19:49 -0200

    create map widgets

--- js/mira/widgets/map.js
+++ js/mira/widgets/map.js
@@ -11,8 +11,8 @@ define([
 
             var element = document.createElement('img');
             element.id = name;
-            var attrs = _.omit(options, 'tag', 'value', 'name', 'widget', 'class', 'alt', 'title', 'wight', 'height');
-            var url = "http://maps.googleapis.com/maps/api/staticmap?";
+            var attrs = _.omit(options, 'tag', 'value', 'name', 'widget', 'class', 'width', 'height');
+            var url = "http://maps.googleapis.com/maps/api/staticmap?sensor=false&";
 
             attrs['center'] = options['value'];
 
@@ -22,7 +22,7 @@ define([
             url += $.param(attrs_compiled);
             element.setAttribute('src', url);
 
-            var element_attributes = _.pick(options, 'class', 'alt', 'title', 'wight', 'height');
+            var element_attributes = _.pick(options, 'class', 'alt', 'title', 'width', 'height');
             Helper.build_attributes(element, element_attributes, context);
 
             $parent.append(element);
@@ -33,14 +33,27 @@ define([
         },
 
         Dynamic: function($parent, name, $data, $env, options){
-            var $head = $('head');
+            var element = document.createElement('div');
+            var $element = $(element);
 
-            if($head.find('.map_lib').length == 0){
-                var js = document.createElement('script')
+            var attrs = _.omit(options, 'tag', 'value', 'name', 'widget', 'class', 'alt', 'title', 'width', 'height');
+            var context = Helper.build_context($data, $env, options);
+            var attrs_compiled = Helper.build_object_with_context(attrs, context);
 
+            require('async!http://maps.google.com/maps/api/js?sensor=false', 'gmaps3', function(){
 
+                $("#test").gmap3({ map: attrs_compiled });
+
+                var element_attributes = _.pick(options, 'class', 'width', 'height');
+                Helper.build_attributes(element, element_attributes, context);
+
+                $parent.append(element);
+            });
+
+            return {
+                $children: $(element),
+                html: element.innerHTML
             }
-
         }
     };
 });
