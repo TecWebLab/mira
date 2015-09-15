c9d98b3c983e52e84300f6e5d711d9a55d3838c7
Ezequiel Bertti <ebertti@gmail.com>
2015-09-15 15:28:33 -0300

    dando mais legibilidade a interface concreta de user

--- js/example/github.js
+++ js/example/github.js
@@ -4,8 +4,8 @@ var sourceUsers = [{
     name: 'Ezequiel Bertti',
     link: 'https://api.github.com/users/ebertti'
     },{
-    name: 'Joao Leite',
-    link: 'https://api.github.com/users/joaoleite'
+    name: 'Magela',
+    link: 'https://api.github.com/users/magelinha'
 }];
 
 var rules = [{
@@ -14,6 +14,9 @@ var rules = [{
 },{
     name: 'isUser',
     validate: '$data.login != null'
+},{
+    name: 'hasDetail',
+    validate: '$bind != null'
 },{
     name: 'haveStar',
     validate: '$data.stargazers_count > 0'
@@ -45,26 +48,38 @@ var interface_abstracts = [
             {'footer': ['footer-content']}
         ]
     },{
-        name: 'user',
-        widgets: [
-            {'navigation': {name:'navigation-list', children:['navigation-list-item'], datasource:sourceUsers}},
-            { name: "content",
-              children: [
-                  {'user': ['avatar', 'name', 'login', 'bio', 'blog', 'company', 'location']},
-                  { 'seguidores_panel': ['seguidores_title',
-                  { name: "seguidores", datasource: "url:<%= $data.followers_url %>", cache: false,
-                      children: {"seguidor": ['avatar_seguidor']}
-                  }, 'seguidores_mais']},
-                  {'repositorios_panel' : ['repositorios_title',
-                  { name: "repositorios", datasource: "url:<%= $data.repos_url %>",
-                      children: {"repositorio": ['nome', 'descricao', 'star', 'watch']}
-                  }]}
-              ]
-            },
-            {'footer': ['footer-content']}
-        ]
+        name:"user",
+        widgets:[ { name:"navigation",
+            children:[ { name:"navigation-list",datasource:sourceUsers,
+                children:[ { name:"navigation-list-item", bind:"$data.name" } ] } ] },
+            { name:"content",
+                children:[ { name:"user",
+                    children:[ { name:"avatar",  bind:"$data.avatar_url" },
+                        { name:"name", bind:"$data.name", when: 'hasDetail'},
+                        { name:"login", bind:"$data.login", when: 'hasDetail' },
+                        { name:"bio", bind:"$data.bio", when: 'hasDetail' },
+                        { name:"blog", bind:"$data.blog", when: 'hasDetail' },
+                        { name:"company", bind:"$data.company", when: 'hasDetail' },
+                        { name:"location", bind:"$data.location", when: 'hasDetail' } ] },
+                    { name:"seguidores_panel",
+                        children:[ { name:"seguidores_title" },
+                            { name:"seguidores", datasource:"url:<%= $data.followers_url %>",
+                                children:[ { name:"seguidor", bind:"$data.url",
+                                    children:[ { name:"avatar_seguidor", bind:"$data.avatar_url" } ] } ]  },
+                            { name:"seguidores_mais" } ] },
+                    { name:"repositorios_panel",
+                        children:[ { name:"repositorios_title" },
+                            { name:"repositorios", datasource:"url:<%= $data.repos_url %>",
+                                children:[ { name:"repositorio",
+                                    children:[ { name:"nome",
+                                        bind:"$data.name" },
+                                        { name:"descricao", bind:"$data.description" },
+                                        { name:"star" },
+                                        { name:"watch", bind:"$data.watchers_count" } ] } ]
+                                } ] } ] },
+            { name:"footer",
+                children:[ { name:"footer-content" } ] } ]
     }
-
 ];
 
 var concrete_interface = [
@@ -122,23 +137,23 @@ var concrete_interface = [
         maps: [
         { name: 'navigation', widget: 'BootstrapNavigation', value:'"GitHub"'},
         { name: 'navigation-list', widget: 'BootstrapNavigationList'},
-        { name: 'navigation-list-item', widget: 'BootstrapNavigationListItem', value:'$data.name', href:'navigate($data.link)'},
+        { name: 'navigation-list-item', widget: 'BootstrapNavigationListItem', value:'$bind', href:'navigate($data.link)'},
 
         { name: 'content', widget: 'ProfileContainer' },
         { name: 'user', widget: 'SimpleHtml', class:'clearfix' },
         { name: 'avatar', widget: 'ProfileImage', value:'$data.avatar_url' },
         { name: 'detail', widget: 'SimpleHtml', class:'col-xs-12 col-sm-8' },
-        { name: 'name', widget: 'SimpleHtml', tag: 'h2', value: '$data.name' },
-        { name: 'login', widget: 'ProfileDetail', detail: 'Login', value: '$data.login'},
-        { name: 'bio', widget: 'ProfileDetail', detail: 'Bio', value: '$data.bio'},
-        { name: 'blog', widget: 'ProfileDetail', detail: 'Blog', value: '$data.blog'},
-        { name: 'company', widget: 'ProfileDetail', detail: 'Company', value: '$data.company'},
-        { name: 'location', widget: 'ProfileDetail', detail: 'Location', value: '$data.location'},
+        { name: 'name', widget: 'SimpleHtml', tag: 'h2', value: '$bind' },
+        { name: 'login', widget: 'ProfileDetail', detail: 'Login', value: '$bind'},
+        { name: 'bio', widget: 'ProfileDetail', detail: 'Bio', value: '$bind'},
+        { name: 'blog', widget: 'ProfileDetail', detail: 'Blog', value: '$bind'},
+        { name: 'company', widget: 'ProfileDetail', detail: 'Company', value: '$bind'},
+        { name: 'location', widget: 'ProfileDetail', detail: 'Location', value: '$bind'},
         { name: 'seguidores_panel', widget: 'SimpleHtml', class:'clearfix' },
         { name: 'seguidores_title', widget: 'SimpleHtml', tag: 'h3', class:'clearfix', value:'"Seguidores"'},
         { name: 'seguidores', widget: 'SimpleHtml', tag: 'div'},
-        { name: 'seguidor', widget: 'SimpleHtml', tag: 'a', href: 'navigate($data.url)'},
-        { name: 'avatar_seguidor', widget: 'SimpleHtml', class:'col-md-2 col-xs-3 img-circle img-responsive', tag: 'img', src: '$data.avatar_url + "s=80"', alt:'$data.login', title:'$data.login'},
+        { name: 'seguidor', widget: 'SimpleHtml', tag: 'a', href: 'navigate($bind)'},
+        { name: 'avatar_seguidor', widget: 'SimpleHtml', class:'col-md-2 col-xs-3 img-circle img-responsive', tag: 'img', src: '$bind + "s=80"', alt:'$data.login', title:'$data.login'},
         { name: 'repositorios_panel', widget: 'SimpleHtml', class:'clearfix' },
         { name: 'repositorios_title', widget: 'SimpleHtml', tag: 'h3', value:'"Repositorios"'},
         { name: 'repositorios', widget: 'SimpleHtml', tag: 'div'},
@@ -146,8 +161,8 @@ var concrete_interface = [
         { name: 'nome', widget: 'SimpleHtml', tag: 'h4', value: '$data.name', class:'media-heading'},
         { name: 'descricao', widget: 'SimpleHtml', tag: 'span', value: '$data.description'},
         { name: 'box', widget: 'SimpleHtml', tag: 'ul', class:'nav nav-pills nav-stacked pull-right'},
-        { name: 'watch', widget: 'ProfileCount', icon:'eye-close', value:'$data.watchers_count'},
-        { name: 'watch', widget: 'ProfileCount', icon:'eye-open', value:'$data.watchers_count', when:'haveWatch'},
+        { name: 'watch', widget: 'ProfileCount', icon:'eye-close', value:'$bind'},
+        { name: 'watch', widget: 'ProfileCount', icon:'eye-open', value:'$bind', when:'haveWatch'},
 
         { name: 'footer', widget: 'SimpleHtml', tag:'div', class:'container' },
         { name: 'footer-content', widget: 'BootstrapFooter' }
